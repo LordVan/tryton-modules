f84963d4fbe29b84c6c1b366b410ab11ea7c4007
Thomas Raschbacher <lordvan@lordvan.com>
2018-09-28 13:44:07 +0200

    First commit

---
+++ sale_list_description/__init__.py
@@ -0,0 +1,8 @@
+from trytond.pool import Pool
+#from .sale import *
+#
+#def register():
+#    Pool.register(
+#        SaleLine,
+#        module='sale_list_description', type_='model',
+#        )
