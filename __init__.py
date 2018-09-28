a54678a260c57bb135866dab2c37072eccd28e36
Thomas Raschbacher <lordvan@lordvan.com>
2018-09-28 13:49:47 +0200

    moved to folder

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from trytond.pool import Pool
-#from .sale import *
-#
-#def register():
-#    Pool.register(
-#        SaleLine,
-#        module='sale_list_description', type_='model',
-#        )
