3dba5772334eaa2654f8c4a43a83f88134f80140
Thomas Raschbacher <lordvan@lordvan.com>
2019-01-06 10:06:01 +0100

    inital commit of extra party module

---
+++ party_extra/party.py
@@ -0,0 +1,18 @@
+from trytond.model import fields
+from trytond.pyson import Eval, Bool, Id
+from trytond.pool import PoolMeta, Pool
+
+__all__ = ['Party']
+
+    
+class Party(metaclass=PoolMeta):
+    __name__ = 'party.party'
+
+    # custom stuff for us
+    # unique could be added by adding sql constraints - see __setup__ in party module party.py
+    customer_no = fields.Char('Customer number', size=9)
+    supplier_no = fields.Char('Supplier number', size=9)
+    pn_name = fields.Char('PN name', size=50)
+    
+    # general stuff
+    #place_of_jurisdiction = fields.Char('Place of jurisdiction', size=100)
