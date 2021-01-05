980bd41fc8f2936558b06641c8e79c9394bb2261
Thomas Raschbacher (LordVan) <lordvan@lordvan.com>
2021-01-05 16:08:29 +0100

    changed module field internal_name to legal_name cuz we are using name for the internal one also added dolibarr contact import

--- dolibarr_import/dolibarr_csv_import.py
+++ dolibarr_import/dolibarr_csv_import.py
@@ -8,6 +8,8 @@ pcfg = config.set_trytond(database='tryton', config_file='/etc/tryton/trytond.co
 
 ENCODING = 'iso-8859-15'
 
+DEBUG = True
+
 #################
 # Generic stuff #
 #################
@@ -22,22 +24,25 @@ Country = Model.get('country.country')
 SubD = Model.get('country.subdivision')
 Cont = Model.get('party.contact_mechanism')
 Ident = Model.get('party.identifier')
+Prel = Model.get('party.relation.all') 
+PrelTyp = Model.get('party.relation.type')
+
+PREL_EMPLOYEE = 'Mitarbeiter'
 
 lang_de, = Lang.find([('code', '=', 'de')]) # default language
 
 def get_or_create_category(cname):
-    pass
+    try:
+        cat, = Categ.find([('name', '=', cname)])
+        # FIXME: assuming there will only be one with that name
+        # when i think that it is possible to have the same name, but not top level
+        return cat
+    except:
+        cat = Categ(name = cname)
+        cat.save()
+        return cat
 
-categname = 'Import 2020-01-04'
-categs = Categ.find([('name','=', categname)])
-categ = None
-if categs:
-    categ = categs[0]
-else:
-    categ = Categ()
-    categ.name =  categname
-    categ.save()
-#print (f'{categ=}')
+
 country_at, = Country.find([('code', '=', 'AT')])
 
 def add_note(res, text, e=None):
@@ -50,6 +55,10 @@ def add_note(res, text, e=None):
     note.save()
 
 
+#########################
+# misc helper functions #
+#########################
+    
 from io import StringIO
 from html.parser import HTMLParser
 
@@ -69,6 +78,23 @@ def strip_tags(html):
     s = MLStripper()
     s.feed(html)
     return s.get_data()
+
+def print_progress(ncount):
+    scount = ' '
+    if ncount % 1000 == 0:
+        scount = 'M'
+    elif ncount % 500 == 0:
+        scount = 'D'
+    elif ncount % 100 == 0:
+        scount = 'C'
+    elif ncount % 50 == 0:
+        scount = 'L'
+    elif ncount % 10 == 0:
+        scount = 'X'
+    else:
+        scount = '.'
+    print(scount, end='', flush = True)
+
 
 ################
 # Party import #
@@ -110,8 +136,7 @@ all_parties=Party.find()
 #     print(p.name)
 #     print(p.addresses[0].full_address
 #     )
-print(f'Found {len(all_parties)} parties. If you still want to import press Enter otherwise abort')
-sys.stdin.readline()
+answer= input(f'Found {len(all_parties)} parties. If you still want to import press Enter otherwise type "no"')
 
 with codecs.open(party_fname, 'r', ENCODING) as fp:
     reader = csv.reader(fp)
@@ -119,46 +144,70 @@ with codecs.open(party_fname, 'r', ENCODING) as fp:
     headers = next(reader)
     print(f'{headers=}')
 
+    ncount = 0
     for row in reader:
         np = Party()
         np.name = row[1].strip()
         np.lang = lang_de # since row[20] is empty ..
-        np.save() # save here so we got basic stuff that shouldn't fail and can attach notes,..
-#        np.categories.append(categ)
+        try:
+            np.dolibarr_pid = int(row[0])
+            np.save() # save here so we got basic stuff that shouldn't fail and can attach notes,..
+        except Exception as e:
+            print(f'\nError adding dolibar_pid {row[0]}.\n\n{e}')
+            continue
+        
+        # try:
+        #     exists = Party.find([('dolibarr_pid', '=', int(row[0]))])
+        #     if not exists:
+        #         # no need to check this needs to be present or the export is bogus
+        #         np.dolibarr_pid = int(row[0])
+        #     else:
+        #         add_note(np, f'Dolibarr party ID <<{row[0]=}>> already exists !!')
+        # except Exception as e:
+        #     add_note(np, f'Error adding dolibarr_pid <<{row[0]=}>>', e)
+            
+        np.categories.append(get_or_create_category('Import 2020-01-05'))
         # Address
         addr = np.addresses[0] # seems like every contact comes witha t least one address
         if row[5] and row[5].strip():
             try:
                 addr.zip = row[5].strip()
             except Exception as e:
-                add_note(np, f'Error adding ZIP with <<{row[5]}>>', e)
+                add_note(np, f'Error adding ZIP with <<{row[5]=}>>', e)
         if row[4] and row[4].strip():
             try:
                 addr.street = row[4].strip()
             except Exception as e:
-                add_note(np, f'Error adding street with <<{row[4]}>>', e)                   
+                add_note(np, f'Error adding street with <<{row[4]=}>>', e)                   
         if row[6] and row[6].strip():
             try:
                 addr.city = row[6].strip()
             except Exception as e:
-                add_note(np, f'Error adding city with <<{row[6]}>>', e)                   
+                add_note(np, f'Error adding city with <<{row[6]=}>>', e)                   
 
         if row[9] and row[9].strip():
             try:
                 addr.country, = Country.find([('code', '=', row[9].strip())])
             except Exception as e:
-                add_note(np, f'Error adding country with code <<{row[9]}>>', e)
-        else:
-            addr.country = country_at
+                add_note(np, f'Error adding country with code <<{row[9]=}>>', e)
+        # default country set later to avoid creating dummy addresses
+        # with just the default country
         if row[7] and row[7].strip():
             try:
                 sd, = SubD.find([('name', '=', row[7].strip())])
                 addr.subdivision = sd
             except Exception as e:
-                add_note(np, f'Error adding country subdivision <<{row[7]}>>', e)
+                add_note(np, f'Error adding country subdivision <<{row[7]=}>>', e)
         # address done .. save
         try:
-            addr.save()
+            # check if we have *any* data
+            if addr.city or addr.street or addr.country or addr.subdivision or addr.zip:
+                if not addr.country:
+                    # if we have an address but no country we set the default
+                    addr.country = country_at
+                addr.save()
+            else:
+                addr.delete() # remove address
         except Exception as e:
             add_note(np, 'Error saving address', e)
 
@@ -166,49 +215,45 @@ with codecs.open(party_fname, 'r', ENCODING) as fp:
         # save after every one to make sure validation triggers
         if row[10] and row[10].strip():
             try:
-                cm = np.contact_mechanisms.new(type='phone')
+                cm = Cont(type='phone')
                 cm.value = row[10].strip()
+                cm.party = np
                 cm.save()
             except Exception as e:
-                add_note(np, f'Error adding phone number <<{row[10]}>>', e)
+                add_note(np, f'Error adding phone number <<{row[10]=}>>', e)
         if row[11] and row[11].strip():
             try:
-                cm = np.contact_mechanisms.new(type='fax')
+                cm = Cont(type='fax')
                 cm.value = row[11].strip()
+                cm.party = np
                 cm.save()
             except Exception as e:
-                add_note(np, f'Error adding fax number <<{row[11]}>>', e)
+                add_note(np, f'Error adding fax number <<{row[11]=}>>', e)
         if row[13] and row[13].strip():
             try:
-                cm = np.contact_mechanisms.new(type='email')
+                cm = Cont(type='email')
                 cm.value = row[13].strip()
+                cm.party = np
                 cm.save()
             except Exception as e:
-                add_note(np, f'Error adding email <<{row[13]}>>', e)
+                add_note(np, f'Error adding email <<{row[13]=}>>', e)
         if row[14] and row[14].strip():
             try:
-                cm = np.contact_mechanisms.new(type='website')
+                cm = Cont(type='website')
                 cm.value = row[14].strip()
+                cm.party = np
                 cm.save()
             except Exception as e:
-                add_note(np, f'Error adding website <<{row[14]}>>', e)
+                add_note(np, f'Error adding website <<{row[14]=}>>', e)
 
         # misc
-        try:
-            exists = Party.find([('dolibarr_id', '=', int(row[0]))])
-            if not exists:
-                # no need to check this needs to be present or the export is bogus
-                np.dolibarr_id = int(row[0])
-            else:
-                add_note(np, f'Dolibarr ID <<{row[0]}>> already exists !!')
-        except Exception as e:
-            add_note(np, f'Error adding dolibarr_id <<{row[0]}>>', e)
         if row[2] and row[2].strip():
             try:
                 np.customer_no = row[2].strip()
             except Exception as e:
-                add_note(np, f'Error adding customer number <<{row[2]}>>', e)
+                add_note(np, f'Error adding customer number <<{row[2]=}>>', e)
         # skipping row[3] Lieferanten-Code as tehre is no data in the whole DataSet
+        iderr = False
         # save after every line for validation purposes
         if row[12] and row[12].strip():
             try:
@@ -217,7 +262,8 @@ with codecs.open(party_fname, 'r', ENCODING) as fp:
                 euvat.party = np # party is required to save
                 euvat.save()
             except Exception as e:
-                add_note(np, f'Error adding EU VAT <<{row[12]}>>', e)
+                iderr = True
+                add_note(np, f'Error adding EU VAT <<{row[12]=}>>', e)
         if row[23] and row[23].strip():
             try:
                 atbus=Ident(type='at_businessid')
@@ -225,26 +271,231 @@ with codecs.open(party_fname, 'r', ENCODING) as fp:
                 atbus.party = np # party is required to save
                 atbus.save()
             except Exception as e:
-                add_note(np, f'Error adding EU VAT <<{row[23]}>>', e)
+                iderr = True
+                add_note(np, f'Error adding EU VAT <<{row[23]=}>>', e)
+        if iderr:
+            np.categories.append(get_or_create_category('ID Falsch'))
         # ignoring 25 (Rechtsform) and 17 (Typ des Partners)
         if row[26] and row[26].strip():
             try:
                 np.pn_name = row[26].strip()
             except Exception as e:
-                add_note(np, f'Error adding PN Name <<{row[26]}>>', e)
+                add_note(np, f'Error adding PN Name <<{row[26]=}>>', e)
         # notes from Dolibarr
+        has_note = False
         if row[15] and row[15].strip() and strip_tags(row[15]).strip():
             add_note(np, 'Notiz (privat):\n\n' + strip_tags(row[15]).strip())
+            has_note = True
         if row[16] and row[16].strip() and strip_tags(row[16]).strip():
             add_note(np, 'Notiz (öffentlich):\n\n' + strip_tags(row[16]).strip())
+            has_note = True
+        if has_note:
+            np.categories.append(get_or_create_category('Notiz'))
 
         # save everything
-        #sys.stdout.write('.')
-        print('.', end='', flush = True)
         try:
             np.save()
         except Exception as e:
             add_note(np, 'Error saving party', e)
+        ncount += 1
+        print_progress(ncount)
 
+
+#
+print('\nParty import done')
+
+######################
+# dolibarr contacts  #
+######################
+
+# CSV columns        
+#  0 .. Kontakt-ID
+#  1 .. Anrede
+#  2 .. Nachname
+#  3 .. Vorname
+#  4 .. Position / Funktion
+#  5 .. Privat
+#  6 .. Adresse
+#  7 .. PLZ
+#  8 .. Stadt
+#  9 .. Bundesland
+# 10 .. Land # friggin french .. again
+# 11 .. Ländercode
+# 12 .. Telefon
+# 13 .. Fax
+# 14 .. Mobile
+# 15 .. E-Mail
+# 16 .. Unternehmen-ID <== match this to the parties
+# 17 .. Firmenname # From company table
+# 18 .. Weitere Email 
+# 19 .. Handy 2
+# 20 .. Akadem. Titel
+# 21 .. Weiteres Telefon
+# 22 .. Weiteres Telefon 2
+# 23 .. PN NAME # from company table
+# 24 .. Status # From Company table
+# 25 .. Kundennummer # From Company Table
+# 26 .. Lieferanten-Code # from company table
+
+party2_fname='data/export_societe_2.csv'
+
+with codecs.open(party2_fname, 'r', ENCODING) as fp:
+    reader = csv.reader(fp)
+
+    headers = next(reader)
+    print(f'{headers=}')
+
+    ncount = 0
+    for row in reader:
+        # TODO: somehow check if there is any point in this being it's own party .. but
+        # but difficult to automate
+        np = Party()
+        np.name = f'{row[2].strip()} {row[3].strip()}'.strip()
+        if row[20] and row[20].strip():
+            np.name += f', {row[20].strip()}'
+        # make an educated guess based on 99% of the entries
+        np.legal_name = f'{row[20].strip()} {row[3].strip()} {row[2].strip()}'.strip()
+        np.lang = lang_de
+        np.salutation = row[1].strip()
+        try:
+            np.dolibarr_cid = int(row[0])
+            np.save()
+        except Exception as e:
+            print(f'\nError adding dolibarr_cid {row[0]}.\n\n{e}')
+            continue
+        np.categories.append(get_or_create_category('Import 2020-01-05'))
+
+        # first add relation to (dolibarr) party
+        try:
+            prelt, = PrelTyp.find([('name', '=', PREL_EMPLOYEE)])
+            pemp, = Party.find([('dolibarr_pid','=', int(row[16]))])
+            nr = Prel(type = prelt)
+            nr.from_ = np
+            nr.to = pemp
+            nr.save()
+        except Exception as e:
+            print(f'Error getting employee party relation type . aborting!\n{e}')
+            break
+            
+        # Address
+        addr = np.addresses[0]
+        if row[7] and row[7].strip():
+            try:
+                addr.zip = row[7].strip()
+            except Exception as e:
+                add_note(np, f'Error adding ZIP with <<{row[7]=}>>', e)
+        if row[6] and row[6].strip():
+            try:
+                addr.street = row[6].strip()
+            except Exception as e:
+                add_note(np, f'Error adding street with <<{row[6]=}>>', e)                   
+        if row[8] and row[8].strip():
+            try:
+                addr.city = row[8].strip()
+            except Exception as e:
+                add_note(np, f'Error adding city with <<{row[8]=}>>', e)                   
+
+        if row[11] and row[11].strip():
+            try:
+                addr.country, = Country.find([('code', '=', row[11].strip())])
+            except Exception as e:
+                add_note(np, f'Error adding country with code <<{row[11]=}>>', e)
+        # default country set later to avoid creating dummy addresses
+        # with just the default country
+        if row[9] and row[9].strip():
+            try:
+                sd, = SubD.find([('name', '=', row[9].strip())])
+                addr.subdivision = sd
+            except Exception as e:
+                add_note(np, f'Error adding country subdivision <<{row[9]=}>>', e)
+        # address done .. save
+        try:
+            # check if we have *any* data
+            if addr.city or addr.street or addr.country or addr.subdivision or addr.zip:
+                if not addr.country:
+                    # if we have an address but no country we set the default
+                    addr.country = country_at
+                addr.save()
+            else:
+                addr.delete() # remove address
+        except Exception as e:
+            add_note(np, 'Error saving address', e)
+
+        # Contact Mechanisms
+        # save after every one to make sure validation triggers
+        if row[12] and row[12].strip():
+            try:
+                cm = Cont(type='phone')
+                cm.value = row[12].strip()
+                cm.party = np
+                cm.save()
+            except Exception as e:
+                add_note(np, f'Error adding phone number <<{row[12]=}>>', e)
+        if row[21] and row[21].strip():
+            try:
+                cm = Cont(type='phone')
+                cm.value = row[21].strip()
+                cm.party = np
+                cm.save()
+            except Exception as e:
+                add_note(np, f'Error adding phone number <<{row[21]=}>>', e)
+        if row[22] and row[22].strip():
+            try:
+                cm = Cont(type='phone')
+                cm.value = row[22].strip()
+                cm.party = np
+                cm.save()
+            except Exception as e:
+                add_note(np, f'Error adding phone number <<{row[22]=}>>', e)
+        if row[14] and row[14].strip():
+            try:
+                cm = Cont(type='mobile')
+                cm.value = row[14].strip()
+                cm.party = np
+                cm.save()
+            except Exception as e:
+                add_note(np, f'Error adding phone number <<{row[14]=}>>', e)
+        if row[19] and row[19].strip():
+            try:
+                cm = Cont(type='mobile')
+                cm.value = row[19].strip()
+                cm.party = np
+                cm.save()
+            except Exception as e:
+                add_note(np, f'Error adding phone number <<{row[19]=}>>', e)
+        if row[13] and row[13].strip():
+            try:
+                cm = Cont(type='fax')
+                cm.value = row[13].strip()
+                cm.party = np
+                cm.save()
+            except Exception as e:
+                add_note(np, f'Error adding fax number <<{row[13]=}>>', e)
+        if row[15] and row[15].strip():
+            try:
+                cm = Cont(type='email')
+                cm.value = row[15].strip()
+                cm.party = np
+                cm.save()
+            except Exception as e:
+                add_note(np, f'Error adding email <<{row[15]=}>>', e)
+        if row[18] and row[18].strip():
+            try:
+                cm = Cont(type='email')
+                cm.value = row[18].strip()
+                cm.party = np
+                cm.save()
+            except Exception as e:
+                add_note(np, f'Error adding email <<{row[18]=}>>', e)
+
+        np.save()
+        ncount += 1
+        print_progress(ncount)
+
+        
+#
+print('\nContact import complete')
+        
+#
 print('Import complete')
         
--- rm_extra_data/party.py
+++ rm_extra_data/party.py
@@ -1,4 +1,4 @@
-from trytond.model import fields
+from trytond.model import fields, Unique
 from trytond.pyson import Eval, Bool, Id
 from trytond.pool import PoolMeta, Pool
 
@@ -10,12 +10,31 @@ class Party(metaclass=PoolMeta):
     
     # custom stuff for us
     # unique could be added by adding sql constraints - see __setup__ in party module party.py
-    customer_no = fields.Char('Customer number', size=9)
-    supplier_no = fields.Char('Supplier number', size=9)
-    pn_name = fields.Char('PN name', size=50)
-
-    internal_name = fields.Char('Internal name', help='Name in the format for internal use')
-    dolibarr_id = fields.Integer('Dolibarr id', help='id from dolibarr for import purposes only')
+    customer_no = fields.Char('Customer number',
+                              size = 9)
+    supplier_no = fields.Char('Supplier number',
+                              size = 9)
+    pn_name = fields.Char('PN name',
+                          size = 50)
+    legal_name = fields.Char('Legal name',
+                             help = 'Legally correct name')
+    salutation = fields.Char('Salutation',
+                             size = 50) # limiting length since more seems useless
+    dolibarr_pid = fields.Integer('Dolibarr party id',
+#                                  readonly = True,
+                                  help = 'party id from dolibarr (for import purposes only)')
+    dolibarr_cid = fields.Integer('Dolibarr contact id',
+#                                 readonly = True,
+                                 help = 'contact id from dolibarr (for import purposes only)')
     # general stuff
     #place_of_jurisdiction = fields.Char('Place of jurisdiction', size=100)
     
+
+    @classmethod
+    def __setup__(cls):
+        super(Party, cls).__setup__()
+        t = cls.__table__()
+        cls._sql_constraints = [
+            ('dolibarrpid_uniq', Unique(t, t.dolibarr_pid), 'dolibarr_id_dolibarrpid_uniq'),
+            ('dolibarrcid_uniq', Unique(t, t.dolibarr_cid), 'dolibarr_id_dolibarrcid_uniq'),
+        ]
