9592af7fa9f44c2b9dd57d0590192662df169653
Thomas Raschbacher (LordVan) <lordvan@lordvan.com>
2021-03-07 21:12:45 +0100

    added my proteus scripts

---
+++ proteus_scripts/tryton_conn.py
@@ -0,0 +1,22 @@
+import myinit
+
+from proteus import config, Model, Wizard, Report
+pcfg = config.set_trytond(database='tryton', config_file='/etc/tryton/trytond.conf')
+Party = Model.get('party.party')
+Addr = Model.get('party.address')
+Cont = Model.get('party.contact_mechanism')
+Note = Model.get('ir.note')
+Lang = Model.get('ir.lang')
+Categ = Model.get('party.category')
+Country = Model.get('country.country')
+SubD = Model.get('country.subdivision')
+Cont = Model.get('party.contact_mechanism')
+Ident = Model.get('party.identifier')
+Prel = Model.get('party.relation.all')
+PrelTyp = Model.get('party.relation.type')
+
+try:
+    prm, = Party.find([('code', '=', '1')])
+except:
+    print('Could not load party with code 1')
+    
