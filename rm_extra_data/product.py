eab8a4941fe4898ac433c180fb4fd700a942baf6
Thomas Raschbacher (LordVan) <lordvan@lordvan.com>
2021-04-05 14:25:29 +0200

    fix copy paste mistake that made everything readonly

--- rm_extra_data/product.py
+++ rm_extra_data/product.py
@@ -11,16 +11,12 @@ class Product(metaclass=PoolMeta):
     __name__ = 'product.product'
 
     material = fields.Char('Material',
-                           states = {'readonly': Eval('sale_state') != 'draft', },
                            help = 'Material name (auto-filled from product)')
     material_extra = fields.Char('Material extra text',
-                                 states = { 'readonly': Eval('sale_state') != 'draft', },
                                  help = 'extra text to be appended to material')
     material_surface = fields.Char('Material surface',
-                                   states = { 'readonly': Eval('sale_state') != 'draft', },
                                    help = 'surface treatment for material (paint,..) ')
     # this is actually also used for non sheet metal thickness, but not going to rename the field, just changing the description
     sheet_thickness = fields.Float('Material thickness',
-                                   states = { 'readonly': Eval('sale_state') != 'draft', },
                                    digits = (2, 2),
                                    help = 'material thickness')
