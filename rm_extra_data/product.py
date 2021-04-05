07956356db2c3a4d87c310e3f87c34f5733a8940
Thomas Raschbacher (LordVan) <lordvan@lordvan.com>
2021-04-05 14:50:34 +0200

    added simple product form view

--- rm_extra_data/product.py
+++ rm_extra_data/product.py
@@ -13,9 +13,9 @@ class Product(metaclass=PoolMeta):
     material = fields.Char('Material',
                            help = 'Material name (auto-filled from product)')
     material_extra = fields.Char('Material extra text',
-                                 help = 'extra text to be appended to material')
+                                 help = 'extra text to be appended to material (usually only needed on sale line)')
     material_surface = fields.Char('Material surface',
-                                   help = 'surface treatment for material (paint,..) ')
+                                   help = 'surface treatment for material (paint,..) (usually only needed on sale line)')
     # this is actually also used for non sheet metal thickness, but not going to rename the field, just changing the description
     sheet_thickness = fields.Float('Material thickness',
                                    digits = (2, 2),
