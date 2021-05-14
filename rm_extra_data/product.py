6dcba160ec43c4a73560d50a159bff511f650b3f
Thomas Raschbacher (LordVan) <lordvan@lordvan.com>
2021-05-14 20:30:07 +0200

    add custom fields to product and also copy form product -> sale_line when product is selected

--- rm_extra_data/product.py
+++ rm_extra_data/product.py
@@ -10,6 +10,24 @@ __all__ = ['Product']
 class Product(metaclass=PoolMeta):
     __name__ = 'product.product'
 
+    real_product = fields.Boolean('Real product',
+                                  help = 'if selected this is a real, final product, not a placeholder')
+    folder_subno = fields.Char('Subfolder',
+                               states = { 'readonly':  Eval('folder_skip', True), },
+                               help = 'Subfolder number (letter)')
+    folder_submax = fields.Char('Subfolder maximum',
+                                states = { 'readonly': Eval('folder_skip', True), },
+                                help = 'Subfolder maximum')
+    folder_skip = fields.Boolean('Skip this on project sheets',
+                                 help = 'if selected this sale line will not show on project sheets')
+    # TODO: should I mahe the project lines readonly on the form too when skipped? ..
+    proj_line0 = fields.Char('Project sheet line 0',
+                             help = 'above first line on the project sheets generated for this sale line')
+    proj_line1 = fields.Char('Project sheet line 1',
+                             #required = True, # not required on product, but only on sale line !!
+                             help = 'first line on the project sheets generated for this sale line')
+    proj_line2 = fields.Char('Project sheet line 2',
+                             help = 'second line on the project sheets generated for this sale line')
     material = fields.Char('Material',
                            help = 'Material name (auto-filled from product)')
     material_extra = fields.Char('Material extra text',
@@ -20,3 +38,28 @@ class Product(metaclass=PoolMeta):
     sheet_thickness = fields.Float('Material thickness',
                                    digits = (2, 2),
                                    help = 'material thickness')
+
+    # extra fields for delivery notes / invoices (where it does not match the normal lines)
+    # line(s)
+
+    inv_skip = fields.Boolean('Skip this whole sale line for invoice / delivery note',
+                              help = 'if selected this sale line will not show on invoices or delivery notes')
+    inv_line0 = fields.Char('Invoice / delivery note line 0',
+                             states = {'readonly': (Eval('inv_line0_skip', True) | Eval('inv_skip', True)), },
+                             help = 'above first line on the invoice / delivery note generated for this sale line.')
+    inv_line0_skip = fields.Boolean('Skip invoice / delivery note line 0',
+                                    states = {'readonly': Eval('inv_skip', True), },
+                                    help = 'if selected invoice line 0 will be ignored')
+    inv_line1 = fields.Char('Invoice / delivery note line 1',
+                            states = { 'readonly': Eval('inv_skip', True), },
+                             help = 'first line on the invoice / delivery note generated for this sale line')
+    inv_line2 = fields.Char('Invoice / delivery note line 2',
+                             states = {'readonly': (Eval('inv_line2_skip', True) | Eval('inv_skip', True)), },
+                             help = 'second line on the invoice / delivery note generated for this sale line')
+    inv_line2_skip = fields.Boolean('Skip invoice / delivery note line 2',
+                                    states = {'readonly': Eval('inv_skip', True), },
+                                    help = 'if selected invoice line 2 will be ignored')
+
+    @classmethod
+    def default_real_product(cls):
+        return True
--- rm_extra_data/sale.py
+++ rm_extra_data/sale.py
@@ -140,18 +140,31 @@ class SaleLine(metaclass=PoolMeta):
     __name__ = 'sale.line'
 
     # extra fields
+    real_product = fields.Boolean('Real product',
+                                  states = { 'invisible': True, },
+                                  help = 'this gets set if data is filled in from an actual product')
     folder_no = fields.Integer('Folder number',
                                required = True,
-                               states = { 'readonly': ((Eval('sale_state') != 'draft') | Eval('folder_skip')), },
+                               states = { 'readonly': ((Eval('sale_state') != 'draft') |
+                                                       Eval('folder_skip')),
+                               },
                                help = 'Folder number')
     folder_subno = fields.Char('Subfolder',
-                               states = { 'readonly': ((Eval('sale_state') != 'draft') | Eval('folder_skip')), },
+                               states = { 'readonly': ((Eval('sale_state') != 'draft') |
+                                                       Eval('folder_skip') |
+                                                       Eval('real_product')),
+                               },
                                help = 'Subfolder number (letter)')
     folder_submax = fields.Char('Subfolder maximum',
-                                states = { 'readonly': ((Eval('sale_state') != 'draft') | Eval('folder_skip')), },
+                                states = { 'readonly': ((Eval('sale_state') != 'draft') |
+                                                        Eval('folder_skip') |
+                                                        Eval('real_product')),
+                                },
                                 help = 'Subfolder maximum')
     folder_skip = fields.Boolean('Skip this on project sheets',
-                                 #states = { 'readonly': Eval('sale_state') != 'draft', },
+                                 states = { 'readonly': ((Eval('sale_state') != 'draft') |
+                                                        Eval('real_product')),
+                                 },
                                  help = 'if selected this sale line will not show on project sheets')
     due_date = fields.Date('Due date',
                            states = { 'readonly': Eval('sale_state') != 'draft', },
@@ -161,31 +174,47 @@ class SaleLine(metaclass=PoolMeta):
                                    states = { 'readonly': Eval('sale_state') != 'draft', },
                                    help = 'Extra text for due date for this sale line (replaces due date extra from project sheet ')
     proj_line0 = fields.Char('Project sheet line 0',
-                             states = { 'readonly': Eval('sale_state') != 'draft', },
+                             states = { 'readonly': ((Eval('sale_state') != 'draft') |
+                                                     Eval('real_product')),
+                             },
                              help = 'above first line on the project sheets generated for this sale line')
     proj_line1 = fields.Char('Project sheet line 1',
                              required = True,
-                             states = { 'readonly': Eval('sale_state') != 'draft', },
+                             states = { 'readonly': ((Eval('sale_state') != 'draft') |
+                                                     Eval('real_product')),
+                             },
                              help = 'first line on the project sheets generated for this sale line')
     proj_line2 = fields.Char('Project sheet line 2',
-                             states = { 'readonly': Eval('sale_state') != 'draft', },
+                             states = { 'readonly': ((Eval('sale_state') != 'draft') |
+                                                     Eval('real_product')),
+                             },
                              help = 'second line on the project sheets generated for this sale line')
     material = fields.Char('Material',
-                           states = {'readonly': (Eval('sale_state') != 'draft') | Eval('product'), },
+                           states = {'readonly': ((Eval('sale_state') != 'draft') |
+                                                  Eval('product') |
+                                                  Eval('real_product')),
+                           },
                            help = 'Material name (auto-filled from product)')
     # those 2 are not usually set on the product (at least not for now so leave them read-write
     # TODO: maybe make them readonly only if values were set from the product
     material_extra = fields.Char('Material extra text',
-                                 states = { 'readonly': Eval('sale_state') != 'draft', },
+                                 states = { 'readonly': ((Eval('sale_state') != 'draft') |
+                                                         Eval('real_product')),
+                                 },
                                  #states = {'readonly': (Eval('sale_state') != 'draft') | Eval('product'), },
                                  help = 'extra text to be appended to material')
     material_surface = fields.Char('Material surface',
-                                   states = { 'readonly': Eval('sale_state') != 'draft', },
+                                   states = { 'readonly': ((Eval('sale_state') != 'draft') |
+                                                           Eval('real_product')),
+                                   },
                                    #states = {'readonly': (Eval('sale_state') != 'draft') | Eval('product'), },
                                    help = 'surface treatment for material (paint,..) ')
     # this is actually also used for non sheet metal thickness, but not going to rename the field, just changing the description
     sheet_thickness = fields.Float('Material thickness',
-                                   states = {'readonly': (Eval('sale_state') != 'draft') | Eval('product'), },
+                                   states = {'readonly': ((Eval('sale_state') != 'draft') |
+                                                          Eval('product') |
+                                                          Eval('real_product')),
+                                   },
                                    digits = (2, 2),
                                    help = 'material thickness')
     
@@ -193,67 +222,98 @@ class SaleLine(metaclass=PoolMeta):
     # line(s)
 
     inv_skip = fields.Boolean('Skip this whole sale line for invoice / delivery note',
+                              states = { 'readonly': ((Eval('sale_state') != 'draft') |
+                                                      Eval('real_product')),
+                              },
                               help = 'if selected this sale line will not show on invoices or delivery notes')
     
     inv_line0 = fields.Char('Invoice / delivery note line 0',
                              states = {
-                                 'readonly': ((Eval('sale_state') != 'draft')
-                                              | Eval('inv_line0_skip')
-                                              | Eval('inv_skip')
+                                 'readonly': ((Eval('sale_state') != 'draft') |
+                                              Eval('inv_line0_skip') |
+                                              Eval('inv_skip') |
+                                              Eval('real_product')
                                  ),
                              },
                              help = 'above first line on the invoice / delivery note generated for this sale line.')
     inv_line0_skip = fields.Boolean('Skip invoice / delivery note line 0',
-                                    states = {'readonly': ((Eval('sale_state') != 'draft')
-                                                           | Eval('inv_skip')),
+                                    states = {'readonly': ((Eval('sale_state') != 'draft') |
+                                                           Eval('inv_skip') |
+                                                           Eval('real_product')),
                                     },
                                     help = 'if selected invoice line 0 will be ignored')
     inv_line1 = fields.Char('Invoice / delivery note line 1',
                              states = {
-                                 'readonly': ((Eval('sale_state') != 'draft')
-                                              | Eval('inv_skip')),
+                                 'readonly': ((Eval('sale_state') != 'draft') |
+                                              Eval('inv_skip') |
+                                              Eval('real_product')),
                              },
                              help = 'first line on the invoice / delivery note generated for this sale line')
     inv_line2 = fields.Char('Invoice / delivery note line 2',
                              states = {
-                                 'readonly': ((Eval('sale_state') != 'draft')
-                                              | Eval('inv_line2_skip')
-                                              | Eval('inv_skip')),
+                                 'readonly': ((Eval('sale_state') != 'draft') |
+                                              Eval('inv_line2_skip') |
+                                              Eval('inv_skip') |
+                                              Eval('real_product')),
                              },
                              help = 'second line on the invoice / delivery note generated for this sale line')
     inv_line2_skip = fields.Boolean('Skip invoice / delivery note line 2',
-                                    states = {'readonly': ((Eval('sale_state') != 'draft')
-                                                           | Eval('inv_skip')),
-                                              },
+                                    states = {'readonly': ((Eval('sale_state') != 'draft') |
+                                                           Eval('inv_skip') |
+                                                           Eval('real_product')),
+                                    },
                                     help = 'if selected invoice line 2 will be ignored')
 
+    @classmethod
+    def default_real_product(cls):
+        return False
+    
     @classmethod
     def default_folder_skip(cls):
         return False
     
     @classmethod
     def default_folder_no(cls):
-        # TODO: maybe use highet existing as default ?
+        # TODO: maybe use highest existing as default ?
         return 1
 
-    @classmethod
-    def default_folder_subcount(cls):
-        return 1
-
-    @fields.depends('material', 'material_extra', 'sheet_thickness', 'material_surface')
+    @fields.depends('material', 'material_extra', 'sheet_thickness', 'material_surface', 'real_product', 'folder_skip')
     def on_change_product(self):
         # TODO: set material and sheet_thickness as readonly normally and only allow overwriting if no product is set?
         super(SaleLine, self).on_change_product()
         if not self.product:
+            #reset real_product to false as the product was removed
+            self.real_product = False
             return
-
-
-        # TODO: evaluate if we want to also overwrite with empty values or not
+        
+        # fields that are always set as soon as a product is chosen:
+        self.real_product = self.product.real_product
+        # overwriting material and material thickness
         self.material = self.product.material
         self.sheet_thickness = self.product.sheet_thickness
-        # those two won't usually be set so do not overwrite with empty values FIXME: good or not?
-        if self.product.material_extra:
+        
+        if not self.product.real_product:
+            # this is a placeholder product not a real one so only copy basic data
+            # those two won't usually be set so do not overwrite with empty values
+            if self.product.material_extra:
+                self.material_extra = self.product.material_extra
+            if self.product.material_surface:
+                self.material_surface = self.product.material_surface
+        else:
+            # copy all data from the product since this is an actual product and nto a placeholder
+            # overwriting everything here even if empty
+            self.folder_subno = self.product.folder_subno
+            self.folder_submax = self.product.folder_submax
+            self.folder_skip = self.product.folder_skip
+            self.proj_line0 = self.product.proj_line0
+            self.proj_line1 = self.product.proj_line1 # TODO check this is not empty otherwise raise error!
+            self.proj_line2 = self.product.proj_line2
             self.material_extra = self.product.material_extra
-        if self.product.material_surface:
             self.material_surface = self.product.material_surface
-            
+            # material and sheet_thickness are set above in any case
+            self.inv_skip = self.product.inv_skip
+            self.inv_line0 = self.product.inv_line0
+            self.inv_line0_skip = self.product.inv_line0_skip
+            self.inv_line1 = self.product.inv_line1
+            self.inv_line2 = self.product.inv_line2
+            self.inv_line2_skip = self.product.inv_line2_skip
